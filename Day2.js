a832136360a7e93d069a26bd61d02dc55e18dde3
João Venturini <57732178+JoaoVenturini09@users.noreply.github.com>
2025-02-28 16:19:11 -0300

    Create Day2.js

---
+++ Day2.js
@@ -0,0 +1,11 @@
+    // Pergunta o nome do usuário
+    let nome = prompt("Qual o seu nome?");
+  
+    // Pergunta a idade do usuário
+    let idade = prompt("Quantos anos você tem?");
+  
+    // Pergunta a linguagem de programação que o usuário está estudando
+    let linguagem = prompt("Qual linguagem de programação você está estudando?");
+  
+    // Exibe a mensagem com as informações fornecidas pelo usuário
+    alert(`Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}!`);
